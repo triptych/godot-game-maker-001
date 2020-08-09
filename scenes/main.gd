d21398b1a7163f5810f19f661ec9784638604294
Andrew Wooldridge <triptych@gmail.com>
2020-08-09 13:52:16 -0700

    Adding ability to delete stuff

--- scenes/main.gd
+++ scenes/main.gd
@@ -11,6 +11,18 @@ var data = {
 		items = []
 	}	
 }
+
+class Item:
+	var type = "text"
+	var content = "Some text"
+	
+var textItem = {
+	label = 'unnamed',
+	type = 'textItem',
+	content = "empty"
+}	
+	
+	
 var reset_data = data.duplicate(true)
 
 var file_name = "untitled"
@@ -78,7 +90,7 @@ func _on_main_update_data(label, value):
 func _on_btn_item_add_pressed():
 	print("btn_item_add_pressed")
 	$CenterContainer/ConfirmationDialog.popup()
-	#$TabContainer/World/ItemList.add_item(str("placeholder", rand_range(0,300)))
+
 	pass # Replace with function body.
 
 
@@ -133,4 +145,55 @@ func _on_dialog_save_file_selected(path):
 func _on_ConfirmationDialog_confirmed():
 	print("Text confirmation dialog accepted")
 	print(str("text: ",$CenterContainer/ConfirmationDialog/VBoxContainer/TextEdit.text))
+	var textString = $CenterContainer/ConfirmationDialog/VBoxContainer/TextEdit.text
+	var nodeName = $CenterContainer/ConfirmationDialog/VBoxContainer/HBoxContainer/edit_node_name.text
+	# var newItem = Item.new()
+	# print(str(newItem))
+	# newItem.content = textString
+	# print(JSON.print(newItem))
+	# print(newItem.get_property_list())
+	# pass # Replace with function body.
+	var newItem = textItem.duplicate(true)
+	print(newItem)
+	newItem["label"] = nodeName
+	newItem["content"] = textString
+	print(newItem)
+	print(data.game_world.items)
+	data.game_world.items.append(newItem)
+	print(data)
+	render_data_to_list()	
+
+
+func render_data_to_list():
+	print("render data to list")
+	#$TabContainer/World/ItemList.add_item(str("placeholder", rand_range(0,300)))
+	$TabContainer/World/ItemList.clear()
+	for i in data.game_world.items :
+		print(i['label'])
+		$TabContainer/World/ItemList.add_item(i.label)	
+	$TabContainer/Source/output.text = str(data)
+
+func delItem(itemName): 
+	print(str("delItem called with itemName:", itemName))
+	var foundIdx = 0
+	for x in data.game_world.items:
+		if x.label == itemName:
+			print("found it!", x.label)
+			break
+		else:
+			foundIdx+=1	
+	
+	data.game_world.items.remove(foundIdx)
+	$TabContainer/Source/output.text = str(data)
+	render_data_to_list()
+
+
+func _on_btn_item_remove_pressed():
+	print("on_btn_item_remove pressed")
+	var itemList = $TabContainer/World/ItemList
+	var selItemArr = itemList.get_selected_items()
+	var selItemName = itemList.get_item_text(selItemArr[0])
+	print(selItemArr)
+	print(selItemName)
+	delItem(selItemName)
 	pass # Replace with function body.
